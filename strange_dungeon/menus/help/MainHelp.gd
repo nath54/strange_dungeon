2ea45bb3e832c298df863adb2e9133f1f20dc1f5
nath54 <cerisara.nathan@gmail.com>
2021-06-06 12:25:32 +0200

    aa

---
+++ strange_dungeon/menus/help/MainHelp.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var color = [0.0,0.0,0.0]
+var speed = 1
+
+var min_c = 0
+var max_c = 50
+
+var bg_style = StyleBoxFlat.new()
+
+func _ready():
+	randomize()
+	$Panel.set('custom_styles/panel', bg_style)
+
+func _process(delta):
+	var x = int(rand_range(0, 3))
+	color[x] = clamp(color[x]+rand_range(-speed, speed+1), min_c, max_c)
+	
+	bg_style.set_bg_color(Color(color[0]/255.0, color[1]/255.0, color[2]/255.0))
+
+
+
+func _on_Bt_Menu_pressed():
+	get_tree().change_scene("res://menus/MainMenu.tscn")
